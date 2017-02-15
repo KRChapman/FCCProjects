cf0ac8ad694b2346f1429f36ac71f2515119cbc1
Kyle Chapman <splash004@gmail.com>
2017-02-15 11:25:33 -0800

    finished board click

--- TicTacToe/script/script.js
+++ TicTacToe/script/script.js
@@ -1,20 +1,9 @@
-
+"use strict";
 (function startGame(){
 
-var board = {
-	top: [],
-	middleHorizontal: [],
-	bottom: [],
-
-	left: [],
-	middleVerticle: [],
-	right: [],
-
-	leftDiagnol: [],
-	rightDiagnol: []
+var board = [[],[],[]];
 
 
-}
 
 
 
@@ -22,43 +11,37 @@ var board = {
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 createBoard(ctx);
-var gamepiece = 'o';
-canvasClick();
-
-
-windowClick(gamepiece);
-
-
+var playerPlaySymbol = 'o';
+var computerPlaySymbol = 'x';
+pickGamePiece();
+//put in x o selector function
 
 
 
-function canvasClick(){
-	console.log('1'+gamepiece);
+
+
+
+
+function addCanvasClick(playSymbol){
+	
 
 canvas.addEventListener('click', function(event){
-							squareClicked(this,event);
-							console.log(gamepiece);
-								gamepiece = 'x';
-console.log('2'+gamepiece);
+							squareClicked(this,event,playSymbol);
+					
+						
+
 						});
 
 }
-function windowClick(gamepiece){
-		console.log('3'+gamepiece);
-window.addEventListener('keydown', function(event){
-							// squareClicked(this,event);
-							console.log(gamepiece);
-						});
 
-}
-function createXndO(x,y,gamepiece){
+function createXndO(x,y,playSymbol){
 	this.x = x;
 	this.y = y;
-	this.gamepiece = gamepiece;
+	this.gamepiece = playSymbol;
 
 }
 
-function processClick(x,y,board,gamepiece){
+function processClick(x,y,playSymbol){
 	var	verticleOne = 133;
 	var verticletwo = 266;
 	var horrizontalOne = 133;
@@ -69,22 +52,30 @@ function processClick(x,y,board,gamepiece){
 	var locSmall = 72;
 	var locMid = 206;
 	var locLarge = 340;
-
+    
+    var row;
+    var col;
 
 
 	if (x < verticleOne && y < horrizontalOne){
 		x = locSmall;
 		y = locSmall;
+		row = 0;
+		col = 0;
 
 	} 
 	else if (x < verticletwo && y < horrizontalOne){
 		x = locMid;
 		 y =  locSmall;
+		row = 0;
+		col = 1;
 	}
 
 	else if (x > verticletwo && y < horrizontalOne){
 		x = locLarge;
 		y = locSmall;
+		row = 0;
+		col = 2;
 
 	} 
 
@@ -92,17 +83,21 @@ function processClick(x,y,board,gamepiece){
 	else if (x < verticleOne && y < horrizontalTwo){
 		x =locSmall;
 		y = locMid;
+		row = 1;
+		col = 0;
 	}
 
 	else if (x < verticletwo && y < horrizontalTwo){
 		x = locMid;
 		y = locMid;
-
+		row = 1;
+		col = 1;
 	}
 	else if (x > verticletwo && y < horrizontalTwo){
 		x = locLarge;
 		y = locMid;
-
+		row = 1;
+		col = 2;
 	}
 
 
@@ -110,29 +105,82 @@ function processClick(x,y,board,gamepiece){
 	else if (x < verticleOne && y > horrizontalTwo) {
 		x = locSmall;
 		y = locLarge;
-
+	    row = 2;
+		col = 0;
 	}
-	else if (x < horrizontalTwo && y > horrizontalTwo) {
+	else if (x < verticletwo && y > horrizontalTwo) {
 		x = locMid;
 		y = locLarge;
-
+	    row = 2;
+		col = 1;
 	}
-	else if (x > horrizontalTwo && y > horrizontalTwo) {
+	else if (x > verticletwo && y > horrizontalTwo) {
 		x = locLarge;
 		y = locLarge;
-
+	    row = 2;
+		col = 2;
 	}
 
-	console.log('x'+x);
-	console.log("y"+y);
+	var gamePiece = new createXndO(x,y,playSymbol);
+	loadBoard(row,col,gamePiece);
+	// top = 'top';
+	// piece = board[+top+];
+	// 	console.log(piece);
 
+
+	// 
+	// console.log(board);
+	// console.log("y"+y);
+    
+	
+		// board.push([gamePiece]);
+	// board[0][0]p
+	
+    function loadBoard(row,col,gamePiece){
+    	board[row][col] = gamePiece;
+	
+    	console.log('r',board);
+    }
 }
+
+
 
 function drawGamePiece(){
 
 }
 
-function createBoard(ctx){
+function pickGamePiece(){
+	var li = document.querySelectorAll('li');
+		var ul = document.querySelector('ul');
+
+	for (let i = 1; i < li.length; i++) {
+		li[i].addEventListener('click', function(event){
+										var playSymbol = this.textContent;
+										
+										console.log(event);	
+										if (playSymbol === 'X'){
+											ul.innerHTML = '<li></li>';
+											addCanvasClick(playSymbol);
+
+											// ul.classList.add("hide_li");
+										}
+										else{
+
+											unbeatableAI('X');
+
+
+										}			
+								});
+
+		// canvas.addEventListener('mousedown', drawStatus.bind(null,true));
+	};
+
+	
+
+	// return playSymbol;
+}
+
+function createBoard(){
 ctx.fillStyle = 'black';
 
 ctx.fillRect(0,0,canvas.width,canvas.height);
@@ -149,19 +197,21 @@ ctx.fillStyle = 'white';
 
 }
 
-function unbeatableAI() {
+function unbeatableAI(computerPlaySymbol) {
 
-
+Symbol
 }
 
-function squareClicked(place,event,board,gamepiece){
-		var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+function squareClicked(place,event,playSymbol){
+
 ctx.fillStyle = 'green';
 	var clickedY;
 	var ClickedX;
 	console.log('x'+ event.offsetX);
 	console.log("y"+event.offsetY);
+	console.log(board);
+
+	processClick(event.offsetX,event.offsetY,playSymbol);
 // processClick( event.offsetX,event.offsetY,gamepiece)
 
 //add draw gamePiece function
@@ -181,13 +231,14 @@ ctx.fillStyle = 'green';
 
 
 
-
-
-
-
-
 })(this);
 
 
 
+// function tester(b){
+// 	console.clear();
+// 	console.log(b);
+// 	b = 3;
+// 	console.log('4 '+b);
 
+// }
